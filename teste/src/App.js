316a2c92cecb00ccd7a0d82adca1e9343153165b
TawanDeodato <tawandeodato94@gmail.com>
2024-07-19 21:50:47 -0300

    Instalando react pela primeira vez

---
+++ teste/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello react</h1>
+      <h6>Estou aprendendo react</h6>
+    </div>
+  );
+}
+
+export default App;
