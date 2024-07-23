a66d430d0b10ada417a911e4cbcf006cd900b5ab
TawanDeodato <tawandeodato94@gmail.com>
2024-07-23 19:56:06 -0300

    Estudos sobre icones em react

--- teste/src/components/layout/Footer.js
+++ teste/src/components/layout/Footer.js
@@ -1,5 +1,23 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons-fa";
+import styles from './Footer.modules.css'
+
 function Footer() {
-    return <footer>Rodapé</footer>
+  return (
+    <footer className={styles.social_list} >
+      <ul>
+        <li>
+          <FaFacebook />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+        <li>
+          <FaLinkedin />
+        </li>
+      </ul>
+      <p>Nosso rodapé</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
