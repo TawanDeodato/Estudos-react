543d8d6e33ff7d353e66e339d7a0fcf6733795f5
TawanDeodato <tawandeodato94@gmail.com>
2024-07-23 14:25:08 -0300

    Estudos sobre react router em react

--- teste/src/App.js
+++ teste/src/App.js
@@ -1,14 +1,27 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Empresa from './pages/Empresa'
+import Contato from './pages/Contato'
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 
 function App() {
 
   return (
     <Router> 
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-      </ul>
+      <NavBar />
+      <Switch>
+        <Router path="/">
+          <Home />
+        </Router>
+        <Router path="/empresa">
+          <Empresa />
+        </Router>
+        <Router path="/contato">
+          <Contato />
+        </Router>
+      </Switch>
+      <Footer />
     </Router>
   );
 }
